d4b055c522c035293c826aafcbb3a8cd16547085
Your Name <you@example.com>
2024-03-19 08:10:24 -0700

    Task 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 import json
-import os
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -35,36 +34,33 @@ class FileStorage:
 
     def new(self, obj):
         """
-        Sets in __objects the obj with key <obj class name>.id.
-        Args:
-            obj: The object to be stored.
-        Returns:
-            None
+        Sets in __objects the obj with key <obj class name>.id
         """
-        obj_cls_name = obj.__class__.__name__
-        key = "{}.{}".format(obj_cls_name, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects["{}.{}".format(type(obj).__name__, obj.id)] = obj
 
-    def all(self):
+    def all(self, cls=None):
         """
         Returns the dictionary __objects.
         Returns:
             dict: The dictionary containing stored objects.
         """
-        return FileStorage.__objects
+        if cls is not None:
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
+        return self.__objects
 
     def save(self):
         """
         Serializes __objects to the JSON file (path: __file_path).
-        Returns:
-            None
         """
-        all_objs = FileStorage.__objects
-        obj_dict = {}
-        for obj in all_objs.keys():
-            obj_dict[obj] = all_objs[obj].to_dict()
-        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
-            json.dump(obj_dict, file)
+        odict = {o: self.__objects[o].to_dict() for o in self.__objects.keys()}
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump(odict, f)
 
     def reload(self):
         """
@@ -75,14 +71,26 @@ class FileStorage:
         Returns:
             None
         """
-        if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
-                try:
-                    obj_dict = json.load(file)
-                    for key, values in obj_dict.items():
-                        class_name, obj_id = key.split('.')
-                        cls = eval(class_name)
-                        instance = cls(**values)
-                        FileStorage.__objects[key] = instance
-                except Exception:
-                    pass
+        try:
+            with open(self.__file_path, "r",  encoding="utf-8") as f:
+                for o in json.load(f).values():
+                    name = o["__class__"]
+                    del o["__class__"]
+                    self.new(eval(name)(**o))
+        except FileNotFoundError:
+            pass
+
+    def delete(self, obj=None):
+        """
+        Delete a given object from __objects, if it exists
+        """
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
+
+    def close(self):
+        """
+        Call the reload method
+        """
+        self.reload()
