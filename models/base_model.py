a7103391f023e90e3ac1a4ed112e199b20548b38
Your Name <you@example.com>
2024-03-20 10:59:31 -0700

    Task 6

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
-import uuid
-from datetime import datetime
 import models
+from uuid import uuid4
+from datetime import datetime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column
+from sqlalchemy import DateTime
+from sqlalchemy import String
 
 
 class BaseModel:
@@ -27,6 +31,11 @@ class BaseModel:
         my_model_dict = my_model.to_dict()
         print(my_model)
     """
+
+    id = Column(String(60), primary_key=True, nullable=False)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """
         __init__ - Initializes a new instance of the BaseModel.
@@ -34,20 +43,15 @@ class BaseModel:
         Usage:
             my_model = BaseModel()
         """
-        time_format = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.utcnow()
-        self.updated_at = datetime.utcnow()
+        self.id = str(uuid4())
+        self.created_at = self.updated_at = datetime.utcnow()
         if kwargs:
             for key, value in kwargs.items():
-                if key == "__class__":
-                    continue
-                elif key == "created_at" or key == "updated_at":
-                    setattr(self, key, datetime.strptime(value, time_format))
-                else:
+                if key == "created_at" or key == "updated_at":
+                    value = datetime.strptime(
+                            value, "%Y-<S-F10>%m-%dT%H:%M:%S.%f")
+                if key != "__class__":
                     setattr(self, key, value)
-
-        models.storage.new(self)
 
     def save(self):
         """
@@ -56,6 +60,7 @@ class BaseModel:
             my_model.save()
         """
         self.updated_at = datetime.utcnow()
+        models.storage.new(self)
         models.storage.save()
 
     def to_dict(self):
@@ -66,12 +71,18 @@ class BaseModel:
         Usage:
             my_model_dict = my_model.to_dict()
         """
-        inst_dict = self.__dict__.copy()
-        inst_dict["__class__"] = self.__class__.__name__
-        inst_dict["created_at"] = self.created_at.isoformat()
-        inst_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict = self.__dict__.copy()
+        my_dict["__class__"] = str(type(self).__name__)
+        my_dict["created_at"] = self.created_at.isoformat()
+        my_dict["updated_at"] = self.updated_at.isoformat()
+        my_dict.pop("_sa_instance_state", None)
+        return my_dict
 
-        return inst_dict
+    def delete(self):
+        """
+        Delete the current instance from storage
+        """
+        models.storage.delete(self)
 
     def __str__(self):
         """
@@ -81,21 +92,6 @@ class BaseModel:
         Usage:
             print(my_model)
         """
-        class_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
-
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(
-            key, type(my_model_json[key]), my_model_json[key]
-        ))
+        d = self.__dict__.copy()
+        d.pop("_sa_instance_state", None)
+        return "[{}] ({}) {}".format(type(self).__name__, self.id, d)
