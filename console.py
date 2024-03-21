b2a1db69e18500c4219d3777fca8b40cc72be9b0
Your Name <you@example.com>
2024-03-21 02:45:38 -0700

    Tasks

--- console.py
+++ console.py
@@ -7,6 +7,7 @@ import cmd
 import shlex
 import re
 import ast
+from models import storage
 from models.base_model import BaseModel
 from models.user import User
 from models.state import State
@@ -14,7 +15,6 @@ from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
-from models import storage
 
 
 def split_curly_braces(e_arg):
