0532ed0afdf578d84c1ab02d2a3308b735bdd78b
Your Name <you@example.com>
2024-04-19 09:06:54 -0700

    Tasks

--- models/state.py
+++ models/state.py
@@ -13,9 +13,8 @@ class State(BaseModel, Base):
     """ State class """
     __tablename__ = "states"
     name = Column(String(128), nullable=False)
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City", backref="state", cascade="all, delete")
-    else:
+    cities = relationship("City", backref="state", cascade="delete")
+    if getenv("HBNB_TYPE_STORAGE") != "db":
         @property
         def cities(self):
             """
@@ -25,7 +24,7 @@ class State(BaseModel, Base):
             relationship between State and City.
             """
             city_list = []
-            for city in models.storage.all(City).values():
+            for city in list(models.storage.all(City).values()):
                 if city.state_id == self.id:
                     city_list.append(city)
             return city_list
